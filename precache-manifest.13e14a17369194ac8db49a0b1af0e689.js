42b382a0c142c5c877b749ac6d2dbf32bbf58eef
Leonor Resende <leonor.16.resende@gmail.com>
2020-06-01 12:49:55 +0100

    Updates

---
+++ precache-manifest.13e14a17369194ac8db49a0b1af0e689.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e64ea4ac16bdedb9deb28d01d228c87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fe977a254ac47e77998",
+    "url": "/static/css/2.808d84c1.chunk.css"
+  },
+  {
+    "revision": "292870eea4beb9b51dc3",
+    "url": "/static/css/main.21038485.chunk.css"
+  },
+  {
+    "revision": "2fe977a254ac47e77998",
+    "url": "/static/js/2.22fd54d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.22fd54d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292870eea4beb9b51dc3",
+    "url": "/static/js/main.da54cb77.chunk.js"
+  },
+  {
+    "revision": "1aa9aa2e31a68ae1e121",
+    "url": "/static/js/runtime-main.79bf7f08.js"
+  },
+  {
+    "revision": "5f013c6e30d1eea620be4f3b23e952d6",
+    "url": "/static/media/Jaldi-Bold.5f013c6e.ttf"
+  },
+  {
+    "revision": "f0884788a6720ae0732c422d174c843c",
+    "url": "/static/media/Jaldi-Regular.f0884788.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "8059fea8d1bf8ed5ab7cfb8d51027860",
+    "url": "/static/media/amsc.8059fea8.jpeg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "fa7d64badb49aca6edd4c4ae04f5cc0e",
+    "url": "/static/media/profile.fa7d64ba.jpg"
+  },
+  {
+    "revision": "976760d85ab0aae2bfdff766359f7135",
+    "url": "/static/media/uom.976760d8.jpg"
+  }
+]);
