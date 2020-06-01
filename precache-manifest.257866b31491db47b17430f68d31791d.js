c728122d06d80808bf356014897f292bee05d609
Leonor Resende <leonor.16.resende@gmail.com>
2020-06-01 13:02:50 +0100

    Updates

---
+++ precache-manifest.257866b31491db47b17430f68d31791d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40022009711e40ed7dc6ed7e22f53a4b",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "1fd56b708b5373bd3a53",
+    "url": "/me/static/css/2.8d00e3f0.chunk.css"
+  },
+  {
+    "revision": "1acf70b78ae36ff93e23",
+    "url": "/me/static/css/main.6ff29617.chunk.css"
+  },
+  {
+    "revision": "1fd56b708b5373bd3a53",
+    "url": "/me/static/js/2.22fd54d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me/static/js/2.22fd54d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1acf70b78ae36ff93e23",
+    "url": "/me/static/js/main.da54cb77.chunk.js"
+  },
+  {
+    "revision": "b83d88667ecb9651ba3b",
+    "url": "/me/static/js/runtime-main.51db6a35.js"
+  },
+  {
+    "revision": "5f013c6e30d1eea620be4f3b23e952d6",
+    "url": "/me/static/media/Jaldi-Bold.5f013c6e.ttf"
+  },
+  {
+    "revision": "f0884788a6720ae0732c422d174c843c",
+    "url": "/me/static/media/Jaldi-Regular.f0884788.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/me/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "8059fea8d1bf8ed5ab7cfb8d51027860",
+    "url": "/me/static/media/amsc.8059fea8.jpeg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/me/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/me/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/me/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/me/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/me/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "fa7d64badb49aca6edd4c4ae04f5cc0e",
+    "url": "/me/static/media/profile.fa7d64ba.jpg"
+  },
+  {
+    "revision": "976760d85ab0aae2bfdff766359f7135",
+    "url": "/me/static/media/uom.976760d8.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.13e14a17369194ac8db49a0b1af0e689.js"
+  "/me/precache-manifest.257866b31491db47b17430f68d31791d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/me/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
